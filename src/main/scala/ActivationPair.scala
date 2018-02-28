ec3d9e39fa6642ae4e47e424c49616daccaf7076
muakasan <muakasan@gmail.com>
2018-02-28 01:40:37 -0600

    Activation Pairs Use UFuncs

--- src/main/scala/ActivationPair.scala
+++ src/main/scala/ActivationPair.scala
@@ -1,2 +1,10 @@
-case class ActivationPair(activation: (Double => Double), derivActivation: (Double => Double))
-
+import breeze.generic.{MappingUFunc, UFunc}
+//https://github.com/scalanlp/breeze/issues/159
+case class ActivationPair[Tag1 <: UFunc with MappingUFunc, Tag2 <: UFunc with MappingUFunc](actFun: Tag1, derFun: Tag2) {
+  def activation[T](x: T)(implicit aFun: UFunc.UImpl[Tag1, T, T]): T = {
+    aFun(x)
+  }
+  def activationDeriv[T](x: T)(implicit dFun: UFunc.UImpl[Tag2, T, T]): T = {
+    dFun(x)
+  }
+}
--- src/main/scala/ActivationPairs.scala
+++ src/main/scala/ActivationPairs.scala
@@ -1,6 +1,25 @@
-import breeze.numerics.sigmoid
+import breeze.generic.{MappingUFunc, UFunc}
+import breeze.numerics._
+
+object sigmoidDeriv extends UFunc with MappingUFunc {
+  implicit object implDouble extends Impl[Double, Double] {
+    def apply(a: Double) = sigmoid(a)*(1-sigmoid(a))
+  }
+}
+
+object identity extends UFunc with MappingUFunc {
+  implicit object implDouble extends Impl[Double, Double] {
+    def apply(a: Double) = a
+  }
+}
+
+object identityDeriv extends UFunc with MappingUFunc {
+  implicit object implDouble extends Impl[Double, Double] {
+    def apply(a: Double) = 1
+  }
+}
 
 object ActivationPairs {
-  def sigmoidPair = ActivationPair((x: Double) => sigmoid(x), (x: Double) => sigmoid(x) * (1 - sigmoid(x)))
-  def identityPair = ActivationPair((x:Double) => x, (_:Double) => 1)
+  def sigmoidPair = ActivationPair(sigmoid, sigmoidDeriv)
+  def identityPair = ActivationPair(identity, identityDeriv)
 }
